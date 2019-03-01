f7224b105e5cb402d3307fd8e237020d50a828b1
Zander Dyer <zanderdyer@Zanders-MacBook-Pro.local>
2019-02-28 19:51:22 -0700

    db update

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,13 @@
 USE adopt_db;
-INSERT INTO pets(petName, petAge, petGender, petImage, petBio, isAdopted)
-VALUES ("Lilly", "Puppy", "F", "https://petharbor.com/get_image.asp?RES=Detail&ID=A594182&LOCATION=AZHS3", "I am a sweet puppy who loves to cuddle and play!", False),
-("Tucker", "Adult", "M", "https://petharbor.com/get_image.asp?RES=Detail&ID=A597369&LOCATION=AZHS1", "I am 71 lbs of greatness! Playing fetch is my favorite thing to do. Will you throw the ball for me?", False),
-("Kona", "Adult", "F", "https://petharbor.com/get_image.asp?RES=Detail&ID=A560612&LOCATION=AZHS2", "I am a little shy when I first meet you. Once I get to know you though I am just as sweet as pie! I would really like to go on hikes and play a lot with my new family!", False),
-("Edmond", "Adult", "M", "https://petharbor.com/get_image.asp?RES=Detail&ID=A596542&LOCATION=AZHS2", "I'm a purrfect 10.3 pounds! I love to be loved and snuggled!", False),
-("Spot", "Adult", "F", "https://petharbor.com/get_image.asp?RES=Detail&ID=A598191&LOCATION=AZHS", "My idea of paradise is a forest made of cat trees!", False),
-("Astro", "Kitten", "M", "https://petharbor.com/get_image.asp?RES=Detail&ID=A599358&LOCATION=AZHS", "I am so fluffy, you might mistake me for a snowball!", False)
+INSERT INTO pets(petName, petAge, petGender, petImage, petBio,)
+VALUES ("Lilly", "Puppy", "F", "https://petharbor.com/get_image.asp?RES=Detail&ID=A594182&LOCATION=AZHS3", "I am a sweet puppy who loves to cuddle and play! I would be good in a home with no other pets"),
+("Tucker", "Adult", "M", "https://petharbor.com/get_image.asp?RES=Detail&ID=A597369&LOCATION=AZHS1", "I am 71 lbs of greatness! Playing fetch is my favorite thing to do. I'd love a big family! Will you throw the ball for me?"),
+("Kona", "Adult", "F", "https://petharbor.com/get_image.asp?RES=Detail&ID=A560612&LOCATION=AZHS2", "I am a little shy when I first meet you. But once I get to know you, I am just as sweet as pie! I would really like to go on hikes and play a lot with my new family! An active family would be the best fit for me!")
+
+
+
+-- ("Edmond", "Adult Cat", "M", "https://petharbor.com/get_image.asp?RES=Detail&ID=A596542&LOCATION=AZHS2", "I'm a purrfect 10 pounds! I love to be loved and snuggled!", False),
+-- ("Spot", "Adult Cat", "F", "https://petharbor.com/get_image.asp?RES=Detail&ID=A598191&LOCATION=AZHS", "My idea of paradise is a forest made of cat trees!", False),
+-- ("Astro", "Kitten", "M", "https://petharbor.com/get_image.asp?RES=Detail&ID=A599358&LOCATION=AZHS", "I am so fluffy, you might mistake me for a snowball!", False)
+
+ --TYPE, SIZE, GOOD WITH-- 
